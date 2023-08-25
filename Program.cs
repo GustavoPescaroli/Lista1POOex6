e8434b90d749e4b671f6c677ff93732d08757dbc
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:51:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaPOOex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar a;
+            a = new Dolar();
+
+            Console.WriteLine("valor em dola");
+            a.setqnt(double.Parse(Console.ReadLine()));
+
+            a.calcular();
+            Console.WriteLine("valor convertido = {0}", a.getReais());
+        }
+    }
+}
